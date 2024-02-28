eec3eefffd438d57d2d2a4ef737600da74607702
rohitdshinde <royshinde7744@gmail.com>
2024-02-28 08:48:47 +0530

    Hospital

---
+++ Webform/Form9.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace Webform
+{
+    partial class Form9
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            label7 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox6 = new TextBox();
+            label9 = new Label();
+            textBox7 = new TextBox();
+            label10 = new Label();
+            textBox8 = new TextBox();
+            label11 = new Label();
+            textBox9 = new TextBox();
+            label12 = new Label();
+            textBox10 = new TextBox();
+            label13 = new Label();
+            textBox11 = new TextBox();
+            label14 = new Label();
+            textBox12 = new TextBox();
+            label15 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Symbol", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(218, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(365, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Hospital OPD Registraction";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox12);
+            groupBox1.Controls.Add(label15);
+            groupBox1.Controls.Add(textBox11);
+            groupBox1.Controls.Add(label14);
+            groupBox1.Controls.Add(textBox10);
+            groupBox1.Controls.Add(label13);
+            groupBox1.Controls.Add(textBox9);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(39, 64);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(749, 402);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Patient details";
+            groupBox1.UseCompatibleTextRendering = true;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(17, 369);
+            label8.Name = "label8";
+            label8.Size = new Size(73, 15);
+            label8.TabIndex = 13;
+            label8.Text = "Blood group";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "O+", "O-", "A+", "AB+" });
+            comboBox2.Location = new Point(119, 361);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            comboBox1.AllowDrop = true;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Male", "Female" });
+            comboBox1.Location = new Point(119, 170);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 11;
+            comboBox1.Text = "Select";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(17, 178);
+            label7.Name = "label7";
+            label7.Size = new Size(45, 15);
+            label7.TabIndex = 10;
+            label7.Text = "Gender";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(119, 315);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(17, 318);
+            label6.Name = "label6";
+            label6.Size = new Size(73, 15);
+            label6.TabIndex = 8;
+            label6.Text = "Date of birth";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(119, 256);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(17, 259);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 15);
+            label5.TabIndex = 6;
+            label5.Text = "Occupation";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(119, 113);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(17, 116);
+            label4.Name = "label4";
+            label4.Size = new Size(81, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Second Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(119, 70);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 15);
+            label3.TabIndex = 2;
+            label3.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(119, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(17, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Registraction Date:";
+            label2.Click += label2_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(501, 37);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 15;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(399, 40);
+            label9.Name = "label9";
+            label9.Size = new Size(49, 15);
+            label9.TabIndex = 14;
+            label9.Text = "Address";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(501, 75);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 17;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(399, 78);
+            label10.Name = "label10";
+            label10.Size = new Size(28, 15);
+            label10.TabIndex = 16;
+            label10.Text = "City";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(501, 118);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 19;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(399, 121);
+            label11.Name = "label11";
+            label11.Size = new Size(50, 15);
+            label11.TabIndex = 18;
+            label11.Text = "Pincode";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(501, 170);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 21;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(392, 178);
+            label12.Name = "label12";
+            label12.Size = new Size(71, 15);
+            label12.TabIndex = 20;
+            label12.Text = "Contact No.";
+            label12.Click += label12_Click;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(501, 233);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 23);
+            textBox10.TabIndex = 23;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(392, 241);
+            label13.Name = "label13";
+            label13.Size = new Size(89, 15);
+            label13.TabIndex = 22;
+            label13.Text = "Hospital Detalis";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(501, 293);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(100, 23);
+            textBox11.TabIndex = 25;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(392, 293);
+            label14.Name = "label14";
+            label14.Size = new Size(70, 15);
+            label14.TabIndex = 24;
+            label14.Text = "Department";
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(500, 342);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(100, 23);
+            textBox12.TabIndex = 27;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(398, 345);
+            label15.Name = "label15";
+            label15.Size = new Size(100, 15);
+            label15.TabIndex = 26;
+            label15.Text = "Consultant Name";
+            // 
+            // Form9
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MediumTurquoise;
+            ClientSize = new Size(800, 478);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form9";
+            Text = "Form9";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label8;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label7;
+        private TextBox textBox9;
+        private Label label12;
+        private TextBox textBox8;
+        private Label label11;
+        private TextBox textBox7;
+        private Label label10;
+        private TextBox textBox6;
+        private Label label9;
+        private TextBox textBox10;
+        private Label label13;
+        private TextBox textBox12;
+        private Label label15;
+        private TextBox textBox11;
+        private Label label14;
+    }
+}
--- Webform/Program.cs
+++ Webform/Program.cs
@@ -13,7 +13,8 @@ namespace Webform
             ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
             //Application.Run(new Form2());
-            Application.Run(new Form3());
+            //Application.Run(new Form3());
+            Application.Run(new Form9());
             //Application.Run(new Form5());
             //Application.Run(new Form5());
         }
